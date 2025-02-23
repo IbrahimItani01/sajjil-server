4e17ae787b27b6c843c83c0b95a83c87e1d27b83
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-23 20:38:37 +0200

    controllers<>embedded the guards into the necessary routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,18 +7,23 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dtos/createTask.dto';
 import { UpdateTaskDto } from './dtos/updateTask.dto';
+import { AuthGuard } from 'src/guard/auth.guard';
+import { OwnerGuard } from 'src/guard/owner.guard';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
-  async create(@Body() createTaskDto: CreateTaskDto) {
-    return this.tasksService.create(createTaskDto);
+  create(@Request() req, @Body() createTaskDto: CreateTaskDto) {
+    return this.tasksService.create(createTaskDto, req.user.sub);
   }
 
   @Get()
@@ -31,6 +36,7 @@ export class TasksController {
     return this.tasksService.findOne(id);
   }
 
+  @UseGuards(AuthGuard, OwnerGuard)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.tasksService.update(id, updateTaskDto);
