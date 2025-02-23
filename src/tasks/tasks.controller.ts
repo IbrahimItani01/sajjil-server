960fbe1a68a2da0fe50a2b2f82b60f7d83585ceb
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-23 17:32:51 +0200

    controllers<>created all CRUD tasks apis' controller method and routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,3 +11,33 @@ import {
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dtos/createTask.dto';
 import { UpdateTaskDto } from './dtos/updateTask.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  async create(@Body() createTaskDto: CreateTaskDto) {
+    return this.tasksService.create(createTaskDto);
+  }
+
+  @Get()
+  async findAll(@Param('userId') userId: string) {
+    return this.tasksService.findAll(userId);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.tasksService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
+    return this.tasksService.update(id, updateTaskDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return this.tasksService.remove(id);
+  }
+}
