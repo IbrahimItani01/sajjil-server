cb0fed5776d29b996abecf4a3b22d98d6d596cb0
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-23 17:39:50 +0200

    services<>created auth service methods

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,65 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { CreateAuthDto } from './dtos/register.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginAuthDto } from './dtos/login.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(createAuthDto: CreateAuthDto) {
+    const { name, email, password } = createAuthDto;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new UnauthorizedException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const token = this.generateToken(user.id, user.email);
+
+    return { message: 'User registered successfully', token };
+  }
+
+  async login(loginAuthDto: LoginAuthDto) {
+    const { email, password } = loginAuthDto;
+
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const token = this.generateToken(user.id, user.email);
+
+    return { message: 'Login successful', token };
+  }
+
+  private generateToken(userId: string, email: string) {
+    const payload = { sub: userId, email };
+    return this.jwtService.sign(payload);
+  }
+}
