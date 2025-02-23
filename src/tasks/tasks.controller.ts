f102b92be595b1d07dfd05ce205bfc1809fe78c0
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-23 17:32:01 +0200

    controllers<>imported requried dtos and service file into task controller

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dtos/createTask.dto';
+import { UpdateTaskDto } from './dtos/updateTask.dto';
