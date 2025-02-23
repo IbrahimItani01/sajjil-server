c9b6690d29426bef8bf6c0e98d203c9e5f445ddf
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-23 17:31:24 +0200

    main<>re-designed the main file to log the server url

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    const port = process.env.PORT ?? 4000;
+    await app.listen(port);
+    console.log(`🚀 Server is running on http://localhost:${port}`);
+  } catch (error) {
+    console.error('❌ Error starting the server:', error);
+    process.exit(1);
+  }
 }
-bootstrap();
+
+void bootstrap();
