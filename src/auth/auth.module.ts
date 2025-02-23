dde2c1243261def8158a1e4b79babc806b6ef142
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-23 20:38:05 +0200

    modules<>provided both guard into the auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthGuard } from 'src/guard/auth.guard';
+import { OwnerGuard } from 'src/guard/owner.guard';
 
 @Module({
   imports: [
@@ -21,7 +23,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
     PrismaModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
-  exports: [AuthService],
+  providers: [AuthService, PrismaService, AuthGuard,OwnerGuard],
+  exports: [AuthService, AuthGuard,OwnerGuard],
 })
 export class AuthModule {}
