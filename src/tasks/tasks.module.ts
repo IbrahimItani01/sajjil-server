31d8eda56d2c95d55b8ba68c1c013cbd611186b9
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-23 20:38:57 +0200

    moduels<>provided the ConfigService in tasks module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,10 +4,13 @@ import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,AuthModule,JwtModule],
   controllers: [TasksController],
-  providers: [TasksService, PrismaService],
+  providers: [TasksService, PrismaService,ConfigService],
 })
 export class TasksModule {}
